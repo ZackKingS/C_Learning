7070aa9a261e567c43ece8d560c2dbdbfbb28896
zack <bczeng@iflytek.com>
2021-01-18 15:55:21 +0800

    初始化

---
+++ 内存管理/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  内存管理
+//
+//  Created by zack on 2021/1/18.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+   
+    char name[100];
+    strcpy(name, "Zara Ali");
+    printf("name: %s\n",name);
+    
+    
+    char *description;
+    /* 动态分配内存 */
+    description = (char *)malloc( 200 * sizeof(char) );
+    strcpy( description, "Zara ali a DPS student in class 10th");
+    printf("Description: %s\n", description );
+    
+    return 0;
+}
