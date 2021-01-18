51b517608db0613ac78d46e186ac2866eaf92fd4
zack <bczeng@iflytek.com>
2021-01-18 16:15:10 +0800

    Initial commit 666

---
+++ topic3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  topic3
+//
+//  Created by zack on 2021/1/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"666");
+    }
+    return 0;
+}
